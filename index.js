dd175e3d4347cb360d8b3c3e6874650b52a33c37
Hamza Javed <hamza@efsol.com.pk>
2022-06-20 22:55:17 +0500

    PORT fixes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
  const app = express();
 
+ const port =    process.env.PORT || 3000;
+
 app.use('/',(res,req)=>{
      res.send("Hello World");
 });
 
- app.listen(3000);
+ app.listen(port);
